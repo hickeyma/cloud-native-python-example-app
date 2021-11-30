806398a5e0730edb372ef05d907efc6c2bf6e8de
Martin Hickey <martin.hickey@ie.ibm.com>
2021-11-30 17:34:10 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     #return render_template("index.html")
-    return "This is Party Marty BSOK python app."
+    return "This is Party Marty. please BSOK python app."
  
 
 @app.route("/healthz")
